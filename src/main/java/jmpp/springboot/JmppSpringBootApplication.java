b00767b8ee0d480626827019edaa49d08377bdf4
softozhik <softozhik@live.ru>
2020-11-21 15:40:38 +0300

    сгенерировал пустой spring boot

---
+++ src/main/java/jmpp/springboot/JmppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package jmpp.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmppSpringBootApplication.class, args);
+    }
+
+}
